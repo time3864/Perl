6966ec479fa65cb8938c25ff4a06e46b2a5569c4
time3864 <chfung2020@yahoo.com>
2014-01-03 17:35:09 +0800

    Regular expression start

--- test_perl4.pl
+++ test_perl4.pl
@@ -8,13 +8,49 @@ goto SKIP01;
  
 SKIP01:
 print "\nTest01\n";
+print "Regular Expressions!\n";
+$_ = "yabba dabba doo";
+if(/abba/){
+print "It matched!\n";
+}
 
+$_ = "lala3";
+if(/\P{Space}/){
+print "The string has no whitespace.\n";
+}else{
+print "The string has some whitespace.\n";
+}
 
+$_ = "lala 3";
+if(/\p{Space}/){
+print "The string has some whitespace.\n";
+}else{
+print "The string has no whitespace.\n";
+}
+if(/\P{Space}/){
+print "The string has some whitespace.\n";
+}else{
+print "The string has no whitespace.\n";
+}
 
+if(/\p{Digit}/){
+print "The string has a digit at least.\n";
+}else{
+print "The string has no digit.\n";
+}
 
+if (/\p{Hex}\p{Hex}/) { 
+print "The string has a pair of hex digits.\n";
+}else{
+print "The string has no hex digits.\n";
+}
 
-
-
+$_= "USB 3.1 SuperspeedPLUS 10 Gb/s";
+if(){
+print "USB 3.1 SuperspeedPLUS 10 Gb/s matches something here.\n";
+}else{
+print "Nothing\n";
+}
 
 
 
